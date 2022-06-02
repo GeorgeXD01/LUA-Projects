bc75ff5e9d226395817b440da3ace05245f50299
GeorgeXD01 <106776320+GeorgeXD01@users.noreply.github.com>
2022-06-02 21:27:07 +0100

    Add files via upload

---
+++ nui/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page "html/index.html"
+
+client_scripts "main.lua"
+
+files {
+    "html/index.html",
+    "html/sytle.css",
+    "html/rest.css",
+    "html/listener.js"
+}
---
+++ nui/html/listener.js
@@ -0,0 +1,14 @@
+$(function(){
+    window.onLoad = function(e) {
+        window.addEventListener("message", (event) => {
+            var item = event.data;
+            if(item !== undefined && item.type === "ui") {
+                if (item.display === true) {
+                    $('#container').show();
+                } else {
+                    $('#container').hide();
+                }
+            }
+        })
+    }
+})
